b639a23128906d4e7fe025e81c7f276e67070420
bharathsimhaks <126157034+bharathsimhaks@users.noreply.github.com>
2025-08-25 15:45:20 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -6,3 +6,10 @@ def add(a, b):
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+def sub(a, b):
+    return a - b
+
+def test_sub():
+    assert add(1, 2) == -1
+    assert add(1, 1) == 0
